da737a0676a592b527a7e6c1780766159db0c1a3
Roland Bär <rbaer@gmx.ch>
2023-04-12 20:18:19 +0200

    send to multiple receivers

--- sendipede.py
+++ sendipede.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import csv
 import smtplib, ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
@@ -9,45 +10,51 @@ port = 1025  # Testserver
 #smtp_server = "smtp.gmail.com"
 smtp_server = "localhost"
 sender_email = "rbaer@gmx.ch"  # Enter your address
-receiver_email = "your@gmail.com"  # Enter receiver address
+receivers = set() # eliminates duplicate addresses
 #password = input("Type your password and press enter: ")
 
-message = MIMEMultipart("alternative")
-message["Subject"] = "multipart test"
-message["From"] = sender_email
-message["To"] = receiver_email
-
-# Create the plain-text and HTML version of your message
-text = """\
-Hi,
-How are you?
-Real Python has many great tutorials:
-www.realpython.com"""
-html = """\
-<html>
-  <body>
-    <p>Hi,<br>
-       How are you?<br>
-       <a href="http://www.realpython.com">Real Python</a> 
-       has many great tutorials.
-    </p>
-  </body>
-</html>
-"""
-
-# Turn these into plain/html MIMEText objects
-part1 = MIMEText(text, "plain")
-part2 = MIMEText(html, "html")
-
-# Add HTML/plain-text parts to MIMEMultipart message
-# The email client will try to render the last part first
-message.attach(part1)
-message.attach(part2)
-
+with open('addresses.csv') as file:
+    reader = csv.reader(file)
+    for email in reader:
+        receivers.add(email[0])
 
 context = ssl.create_default_context()
 #with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
 with smtplib.SMTP(smtp_server, port) as server:
     #server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message.as_string())
+
+    for receiver_email in receivers:
+      message = MIMEMultipart("alternative")
+      message["Subject"] = "multipart test"
+      message["From"] = sender_email
+
+      # Create the plain-text and HTML version of your message
+      text = """\
+      Hi,
+      How are you?
+      Real Python has many great tutorials:
+      www.realpython.com"""
+      html = """\
+      <html>
+        <body>
+          <p>Hi,<br>
+            How are you?<br>
+            <a href="http://www.realpython.com">Real Python</a> 
+            has many great tutorials.
+          </p>
+        </body>
+      </html>
+      """
+
+      # Turn these into plain/html MIMEText objects
+      part1 = MIMEText(text, "plain")
+      part2 = MIMEText(html, "html")
+
+      # Add HTML/plain-text parts to MIMEMultipart message
+      # The email client will try to render the last part first
+      message.attach(part1)
+      message.attach(part2)
+
+      message["To"] = receiver_email
+      server.sendmail(sender_email, receiver_email, message.as_string())
 
