787b816fd9c2c6be7ee3ed1779518ab2d0f7ccf8
Roland Bär <rbaer@gmx.ch>
2023-04-14 21:52:47 +0200

    'Feature complete'
    - attachment support
    - message (and subject) from file
    - ssl support or plain (for test system) via config file
    - tested with gmx

--- sendipede.py
+++ sendipede.py
@@ -3,6 +3,8 @@
 import argparse
 import csv
 import smtplib, ssl
+from email import encoders
+from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import yaml
@@ -14,55 +16,68 @@ receivers = set() # eliminates duplicate addresses
 
 parser = argparse.ArgumentParser(description="Sends a message to multiple receivers, but each message individual")
 parser.add_argument("-c", "--config", help="name of the config file (default: %(default)s)", default=config_file)
-parser.add_argument("-a", "--addresses", help="name of the addresses file (default: %(default)s)", default=address_file)
+parser.add_argument("-r", "--receivers", help="name of the file with the receivers addresses, one email address per line (default: %(default)s)", default=address_file)
+parser.add_argument("-a", "--attach", help="file(s) to attach", nargs="*")
+parser.add_argument("message", help="name of the file containing the message to send. The first line in the file is used as the subject of the mail, the rest as the message itself.")
 args = parser.parse_args()
 
 with open(args.config) as file:
   config = yaml.safe_load(file)
 
-with open(args.addresses) as file:
+with open(args.receivers) as file:
   reader = csv.reader(file)
   for email in reader:
     receivers.add(email[0])
 
-context = ssl.create_default_context()
-#with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-with smtplib.SMTP(config['server']['name'], config['server']['port']) as server:
+if(config['server']['ssl']):
+  context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+  context.load_default_certs()
+  server = smtplib.SMTP_SSL(config['server']['name'], config['server']['port'], context=context)
+else:
+  server = smtplib.SMTP(config['server']['name'], config['server']['port'])
+
+try:
   sender = config['sender']
-  #server.login(sender, config['server']['password'])
+
+  if(config['server']['ssl']):
+    server.login(sender, config['server']['password'])
+
+  # Create the plain-text and HTML version of your message
+  with open(args.message) as file:
+    text = file.read()
+
+  texts = text.split('\n', 1)
 
   for receiver in receivers:
-    message = MIMEMultipart("alternative")
-    message["Subject"] = "multipart test"
+    # message = MIMEMultipart("alternative")
+    message = MIMEMultipart()
     message["From"] = sender
-
-    # Create the plain-text and HTML version of your message
-    text = """\
-    Hi,
-    How are you?
-    Real Python has many great tutorials:
-    www.realpython.com"""
-    html = """\
-    <html>
-      <body>
-        <p>Hi,<br>
-          How are you?<br>
-          <a href="http://www.realpython.com">Real Python</a> 
-          has many great tutorials.
-        </p>
-      </body>
-    </html>
-    """
-
-    # Turn these into plain/html MIMEText objects
-    part1 = MIMEText(text, "plain")
-    part2 = MIMEText(html, "html")
-
-    # Add HTML/plain-text parts to MIMEMultipart message
-    # The email client will try to render the last part first
-    message.attach(part1)
-    message.attach(part2)
-
     message["To"] = receiver
+
+    message["Subject"] = texts[0]
+
+    part1 = MIMEText(texts[1], "plain")
+    message.attach(part1)
+
+    if args.attach != None:
+      for attachment_name in args.attach:
+        with open(attachment_name, "rb") as attachment:
+          part = MIMEBase("application", "octet-stream")
+          part.set_payload(attachment.read())
+
+        # Encode file in ASCII characters to send by email
+        encoders.encode_base64(part)
+
+        # Add header as key/value pair to attachment part
+        part.add_header(
+          "Content-Disposition",
+          f"attachment; filename= {attachment_name}",
+        )
+
+        message.attach(part)
+
     server.sendmail(sender, receiver, message.as_string())
+
+finally:
+  server.close()
 
