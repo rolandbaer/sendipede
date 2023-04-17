5adb9d0c17af7aed3658af4fae20ce285d13a763
Roland Bär <rbaer@gmx.ch>
2023-04-17 21:18:33 +0200

    more refactoring

--- sendipede.py
+++ sendipede.py
@@ -12,105 +12,107 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import yaml
 
-config_file = 'config.yml'
-address_file = 'addresses.csv'
+default_config_file = 'config.yml'
+default_address_file = 'addresses.csv'
 
 
 def load_message(filename: str):
-    with open(filename) as file:
-        text = file.read()
+    with open(filename, encoding="utf-8") as _file:
+        _text = _file.read()
 
-    texts = text.split('\n', 1)
-    subject = texts[0]
-    body = texts[1]
-    return subject, body
+    _texts = _text.split('\n', 1)
+    _subject = _texts[0]
+    _body = _texts[1]
+    return _subject, _body
 
 
 def init_parser(config_file: str, address_file: str):
-    parser = argparse.ArgumentParser(
+    _parser = argparse.ArgumentParser(
         description="Sends a message to multiple receivers, but each message individual")
-    parser.add_argument(
+    _parser.add_argument(
         "-c", "--config", help="name of the config file (default: %(default)s)", default=config_file)
-    parser.add_argument(
+    _parser.add_argument(
         "-r", "--receivers", help="name of the file with the receivers addresses, one email address per line (default: %(default)s)", default=address_file)
-    parser.add_argument("-a", "--attach", help="file(s) to attach", nargs="*")
-    parser.add_argument(
+    _parser.add_argument("-a", "--attach", help="file(s) to attach", nargs="*")
+    _parser.add_argument(
         "message", help="name of the file containing the message to send. The first line in the file is used as the subject of the mail, the rest as the message itself.")
-    return parser
+    return _parser
 
 
-receivers = set()  # eliminates duplicate addresses
+if __name__ == "__main__":
+    receivers = set()  # eliminates duplicate addresses
 
-# initialise logging
-logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S',
-                    level=logging.INFO, filename="sendipede {d}.log".format(d=time.strftime("%Y-%m-%d")))
+    # initialise logging
+    logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S',
+                        level=logging.INFO, filename="sendipede {d}.log".format(d=time.strftime("%Y-%m-%d")))
 
-parser = init_parser(config_file, address_file)
-args = parser.parse_args()
+    parser = init_parser(default_config_file, default_address_file)
+    args = parser.parse_args()
 
-logging.info("starting Sendipede")
-with open(args.config) as file:
-    config = yaml.safe_load(file)
+    logging.info("starting Sendipede")
+    with open(args.config, encoding="utf-8") as file:
+        config = yaml.safe_load(file)
 
-with open(args.receivers) as file:
-    reader = csv.reader(file)
-    for email in reader:
-        receivers.add(email[0])
-
-if (config['server']['ssl']):
-    context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-    context.load_default_certs()
-    server = smtplib.SMTP_SSL(
-        config['server']['name'], config['server']['port'], context=context)
-else:
-    server = smtplib.SMTP(config['server']['name'], config['server']['port'])
-
-try:
-    sender = config['sender']
+    with open(args.receivers, encoding="utf-8") as file:
+        reader = csv.reader(file)
+        for email in reader:
+            receivers.add(email[0])
 
     if (config['server']['ssl']):
-        server.login(sender, config['server']['password'])
+        context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+        context.load_default_certs()
+        server = smtplib.SMTP_SSL(
+            config['server']['name'], config['server']['port'], context=context)
+    else:
+        server = smtplib.SMTP(
+            config['server']['name'], config['server']['port'])
 
-    subject, body = load_message(args.message)
+    try:
+        sender = config['sender']
 
-    logging.info(f'sending message "{subject}"')
+        if (config['server']['ssl']):
+            server.login(sender, config['server']['password'])
 
-    for receiver in receivers:
-        # message = MIMEMultipart("alternative")
-        message = MIMEMultipart()
-        message["From"] = sender
-        message["To"] = receiver
+        subject, body = load_message(args.message)
 
-        message["Subject"] = subject
+        logging.info('sending message "%s"', subject)
 
-        part1 = MIMEText(body, "plain")
-        message.attach(part1)
+        for receiver in receivers:
+            # message = MIMEMultipart("alternative")
+            message = MIMEMultipart()
+            message["From"] = sender
+            message["To"] = receiver
 
-        if args.attach != None:
-            for attachment_name in args.attach:
-                with open(attachment_name, "rb") as attachment:
-                    part = MIMEBase("application", "octet-stream")
-                    part.set_payload(attachment.read())
+            message["Subject"] = subject
 
-                # Encode file in ASCII characters to send by email
-                encoders.encode_base64(part)
+            part1 = MIMEText(body, "plain")
+            message.attach(part1)
 
-                # Add header as key/value pair to attachment part
-                part.add_header(
-                    "Content-Disposition",
-                    f"attachment; filename= {attachment_name}",
-                )
+            if args.attach != None:
+                for attachment_name in args.attach:
+                    with open(attachment_name, "rb") as attachment:
+                        part = MIMEBase("application", "octet-stream")
+                        part.set_payload(attachment.read())
 
-                message.attach(part)
+                    # Encode file in ASCII characters to send by email
+                    encoders.encode_base64(part)
 
-        try:
-            result = server.sendmail(sender, receiver, message.as_string())
-            logging.info(f"message sent to {receiver}")
-        except Exception as exception:
-            logging.error(
-                f"{type(exception)=}: {exception=} while sending to {receiver}")
+                    # Add header as key/value pair to attachment part
+                    part.add_header(
+                        "Content-Disposition",
+                        f"attachment; filename= {attachment_name}",
+                    )
 
-finally:
-    server.close()
+                    message.attach(part)
 
-logging.info("Sendipede stopped")
+            try:
+                result = server.sendmail(sender, receiver, message.as_string())
+                logging.info("message sent to %s", receiver)
+            except Exception as exception:
+                logging.error(
+                    "%s: %s while sending to %s", type(exception), exception, receiver)
+
+    finally:
+        server.close()
+
+    logging.info("Sendipede stopped")
