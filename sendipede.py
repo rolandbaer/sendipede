b78a958747986086f7c3da8c9d0248d8794e00b6
Roland Bär <rbaer@gmx.ch>
2024-10-19 17:32:11 +0200

    refactoring
    fixing issues found by pylint

--- sendipede.py
+++ sendipede.py
@@ -3,7 +3,7 @@
 A script to send messages to multiple recipients, but for each recipient as an
 individual message.
 """
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
 import argparse
 import csv
@@ -18,11 +18,12 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import yaml
 
-default_config_file = 'config.yml'
-default_address_file = 'addresses.csv'
+DEFAULT_CONFIG_FILE = 'config.yml'
+DEFAULT_ADDRESS_FILE = 'addresses.csv'
 
 
 def load_message(filename: str):
+    """Load the message from the given file and return subject and body of the mail message."""
     with open(filename, encoding="utf-8") as _file:
         _text = _file.read()
 
@@ -33,23 +34,28 @@ def load_message(filename: str):
 
 
 def init_parser(config_file: str, address_file: str):
+    """Initialize the argument parser and return it."""
     _parser = argparse.ArgumentParser(
         description="Sends a message to multiple receivers, but each message individual")
     _parser.add_argument(
-        "-c", "--config", help="name of the config file (default: %(default)s)", default=config_file)
+        "-c", "--config", help="name of the config file (default: %(default)s)", \
+            default=config_file)
     _parser.add_argument(
-        "-r", "--receivers", help="name of the file with the receivers addresses, one email address per line (default: %(default)s)", default=address_file)
+        "-r", "--receivers", help="name of the file with the receivers addresses," \
+            " one email address per line (default: %(default)s)", default=address_file)
     _parser.add_argument("-a", "--attach", help="file(s) to attach", nargs="*")
     _parser.add_argument(
-        "message", help="name of the file containing the message to send. The first line in the file is used as the subject of the mail, the rest as the message itself.")
+        "message", help="name of the file containing the message to send. The first " \
+            "line in the file is used as the subject of the mail, the rest as the message itself.")
     _parser.add_argument('-V', '--version', help="Show version information and quit.",
                         action='version', version='Sendipede version ' + __version__)
     return _parser
 
 
 def send_message(subject, body, receivers, args, config):
+    """send the message to the given receivers."""
     logging.info("Start sending Session")
-    if (config['server']['ssl']):
+    if config['server']['ssl']:
         context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
         context.load_default_certs()
         server = smtplib.SMTP_SSL(
@@ -61,7 +67,7 @@ def send_message(subject, body, receivers, args, config):
     try:
         sender = config['sender']
 
-        if (config['server']['ssl']):
+        if config['server']['ssl']:
             server.login(sender, config['server']['password'])
 
         for receiver in receivers:
@@ -75,28 +81,16 @@ def send_message(subject, body, receivers, args, config):
             part1 = MIMEText(body, "plain")
             message.attach(part1)
 
-            if args.attach != None:
-                for attachment_name in args.attach:
-                    with open(attachment_name, "rb") as attachment:
-                        part = MIMEBase("application", "octet-stream")
-                        part.set_payload(attachment.read())
-
-                    # Encode file in ASCII characters to send by email
-                    encoders.encode_base64(part)
-
-                    attachment_filename = os.path.basename(attachment_name)
-                    # Add header as key/value pair to attachment part
-                    part.add_header(
-                        "Content-Disposition",
-                        f"attachment; filename= {attachment_filename}",
-                    )
-
-                    message.attach(part)
+            if args.attach is not None:
+                append_attachments(args, message)
 
             try:
-                result = server.sendmail(sender, receiver, message.as_string())
+                # as we only send to one receiver the result can be ignored
+                server.sendmail(sender, receiver, message.as_string())
                 logging.info("message sent to %s", receiver)
-            except Exception as exception:
+            except (smtplib.SMTPRecipientsRefused, smtplib.SMTPHeloError, \
+                    smtplib.SMTPSenderRefused, smtplib.SMTPDataError, \
+                    smtplib.SMTPNotSupportedError) as exception:
                 logging.error(
                     "%s: %s while sending to %s", type(exception), exception, receiver)
 
@@ -104,20 +98,34 @@ def send_message(subject, body, receivers, args, config):
         server.close()
         logging.debug("Session closed")
 
+def append_attachments(args, message):
+    """append the attachments to the message."""
+    for attachment_name in args.attach:
+        with open(attachment_name, "rb") as attachment:
+            part = MIMEBase("application", "octet-stream")
+            part.set_payload(attachment.read())
+
+                    # Encode file in ASCII characters to send by email
+        encoders.encode_base64(part)
+
+        attachment_filename = os.path.basename(attachment_name)
+                    # Add header as key/value pair to attachment part
+        part.add_header(
+                        "Content-Disposition",
+                        f"attachment; filename= {attachment_filename}",
+                    )
+
+        message.attach(part)
+
 def split_receivers(receivers, session):
+    """split the list of receivers into chunks that fit into a mailserver session."""
     return [receivers[i:i + session] for i in range(0, len(receivers), session)]
 
-if __name__ == "__main__":
+#load_message, send_message, split_receivers,
+def handle_message(args):
+    """main function that handles the message."""
     receivers = set()  # eliminates duplicate addresses
 
-    parser = init_parser(default_config_file, default_address_file)
-    args = parser.parse_args()
-
-    # initialise logging
-    logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S',
-                        level=logging.INFO, filename="sendipede-{d}.log".format(d=time.strftime("%Y-%m-%d")))
-
-    logging.info("starting Sendipede " + __version__)
     with open(args.config, encoding="utf-8") as file:
         config = yaml.safe_load(file)
 
@@ -138,4 +146,16 @@ if __name__ == "__main__":
     for receiver_chunk in receivers_chunks:
         send_message(subject, body, receiver_chunk, args, config)
 
+if __name__ == "__main__":
+    parser = init_parser(DEFAULT_CONFIG_FILE, DEFAULT_ADDRESS_FILE)
+    arguments = parser.parse_args()
+
+    # initialise logging
+    logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S',
+                        level=logging.INFO,
+                        filename=f"sendipede-{time.strftime('%Y-%m-%d')}.log")
+
+    logging.info("starting Sendipede %s", __version__)
+    handle_message(arguments)
+    #load_message, send_message, split_receivers,
     logging.info("Sendipede stopped")
